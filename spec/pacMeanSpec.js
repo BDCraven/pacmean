ac20e939800050a807cfb0c2a11c9754c02d14a0
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-30 13:06:03 +0100

    pacmean object now moves in response to holding down the keys

--- spec/pacMeanSpec.js
+++ spec/pacMeanSpec.js
@@ -8,6 +8,7 @@ describe("PacMean", function () {
   var pacMean;
 
   beforeEach(function () {
+    spyOn(PacMean.prototype, "draw")
     pacMean = new PacMean(10, 120);
   });
 
@@ -19,5 +20,33 @@ describe("PacMean", function () {
   it("starts at set co-ordinates", function () {
     expect(pacMean.x).toEqual(10);
     expect(pacMean.y).toEqual(120);
+  });
+
+  it("has a default speed", function () {
+    expect(pacMean.SPEED).toEqual(1);
+  });
+
+  it("calls its draw function on instantiation", function () {
+    expect(pacMean.draw).toHaveBeenCalled();
+  });
+
+  it("can have its x co-ordinate increased", function () {
+    pacMean.goRight();
+    expect(pacMean.x).toEqual(11);
+  });
+
+  it("can have its x co-ordinate decreased", function () {
+    pacMean.goLeft();
+    expect(pacMean.x).toEqual(9);
+  });
+
+  it("can have its y co-ordinate decreased", function () {
+    pacMean.goUp();
+    expect(pacMean.y).toEqual(119);
+  });
+
+  it("can have its y co-ordinate increased", function () {
+    pacMean.goDown();
+    expect(pacMean.y).toEqual(121);
   });
 });
