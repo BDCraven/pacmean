95cf444b283439692f18a29856c6905c9231d968
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-30 20:40:18 +0100

    Minor refactoring and added comments for clarity

--- src/interface.js
+++ src/interface.js
@@ -3,15 +3,17 @@ context = canvas.getContext('2d');
 
 window.onload=function() {
   var key;
-  var pacMean;
-  var pacMeanX = 9 * 25;
-  var pacMeanY = 16 * 25;
-  var world = new World;
+  var world, pacMean;
+
+  var pacMeanX = 9 * 25;    //starts at grid position j = 9; change 25 to the constant defined in World
+  var pacMeanY = 16 * 25;   //starts at grid position i = 16; change 25 to the constant defined in World
 
   context.fillStyle='black';
   context.fillRect(0,0,canvas.width,canvas.height);
 
+  world = new World;
   pacMean = new PacMean(pacMeanX, pacMeanY);
+
   window.addEventListener('keydown', function (e) {
     key = e.keyCode;
   });
@@ -26,10 +28,10 @@ window.onload=function() {
   }
 
   function updateGameArea() {
-     if (key && key == 37) {pacMean.goLeft();}
-     if (key && key == 38) {pacMean.goUp();}
-     if (key && key == 39) {pacMean.goRight();}
-     if (key && key == 40) {pacMean.goDown();}
-     clear();
-   }
+    if (key && key == 37) {pacMean.goLeft();}
+    else if (key && key == 38) {pacMean.goUp();}
+    else if (key && key == 39) {pacMean.goRight();}
+    else if (key && key == 40) {pacMean.goDown();}
+    clear();
+  }
 };
