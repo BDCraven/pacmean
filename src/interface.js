a2eca45730dd0765f65e79a30dc22a184981db2f
Imogen Hardy <imogen.hardy@gmail.com>
2017-08-31 16:57:00 +0100

    added pacman theme playing when the window loads

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,8 @@ canvas = document.getElementById('global-canvas');
 context = canvas.getContext('2d');
 
 window.onload=function() {
+  var openingTheme = new Audio('assets/pacman_beginning.wav');
+  openingTheme.play();
   var gameController = new GameController();
   window.addEventListener('keydown', function (e) {
     if (e.keyCode >= 37 && e.keyCode <= 40)
