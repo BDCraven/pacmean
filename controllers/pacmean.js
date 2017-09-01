80c348af53ce9cd55188e76e098a0fb2f5e51e4c
Imogen Hardy <imogen.hardy@gmail.com>
2017-09-01 12:22:56 +0100

    encapsulated sprite class

--- controllers/pacmean.js
+++
@@ -1,9 +0,0 @@
-var world = new World;
-
-canvas = document.getElementById('global-canvas');
-context = canvas.getContext('2d');
-
-context.fillStyle='black';
-context.fillRect(0,0,canvas.width,canvas.height);
-
-world.display();
